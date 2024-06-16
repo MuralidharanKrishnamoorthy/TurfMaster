0330fececa98cdb3eae6067647db5568ebb9412b
Muralidharan K <muralidharansrec@gmail.com>
2024-06-16 12:42:40 +0530

    Initial commit

--- lib/pages-admin/adminregister.dart
+++ lib/pages-admin/adminregister.dart
@@ -2,7 +2,6 @@ import"package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:google_fonts/google_fonts.dart";
 import "package:turfbooking/colors/colors.dart";
-import 'package:otp_text_field/otp_field.dart';
 import "package:turfbooking/pages-admin/admindashboard.dart";
 class adminregister extends StatefulWidget {
   const adminregister({super.key});
